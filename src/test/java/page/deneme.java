e6bd9ada6bc498df7b5f56828017d311284b1b68
taha <tahaacar252@gmail.com>
2023-10-25 00:04:57 +0300

    taha

--- src/test/java/page/deneme.java
+++ src/test/java/page/deneme.java
@@ -4,6 +4,21 @@ public class deneme {
 
 
     public static void main(String[] args) {
+
         System.out.println("team 5 proje");
+        System.out.println("ben taha");
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
