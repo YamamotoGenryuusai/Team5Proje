a779b1566c90e795b9ed11f35899160150e04188
CemilOzer <cemilozer73@gmail.com>
2023-10-25 00:05:03 +0300

    olacak

--- src/test/java/page/deneme.java
+++ src/test/java/page/deneme.java
@@ -1,9 +1,12 @@
 package page;
 
+import java.util.SortedMap;
+
 public class deneme {
-
-
     public static void main(String[] args) {
         System.out.println("team 5 proje");
+        System.out.println("Merhaba arkadaşlar");
+        System.out.println("deneme3");
+
     }
 }
