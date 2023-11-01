4055425f82544cb15f1278553b6f8fc4f760ed00
YamamotoGenryuusai <rasitgover@hotmail.com>
2023-11-01 20:22:40 +0300

    testcommit

--- src/test/java/tests/ozgur/US_019.java
+++ src/test/java/tests/ozgur/US_019.java
@@ -1,16 +1,50 @@
 package tests.ozgur;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import utilities.*;
 
 
-public class US_019 {
+public class US_019 extends TestBaseRapor{
 
 
-@Test
-    public void girisTesti()
+    @Test
+    public void test_01()
 {
+   extentTest=extentReports.createTest("Test1","The user should be able to access the main page of the site.");
    Driver.getDriver().get(ConfigReader.getProperty("smartCardLinkUrl"));
+   extentTest.info("//1-Sign in butonuna tıkla");
+
+    HomePage homePage = new HomePage();
+    homePage.signInButton.click();
+
+    extentTest.info("2-Email gir");
+    homePage.emailtextBox.sendKeys(ConfigReader.getProperty("scAdminEmail"));
+
+
+    extentTest.info("3-Password gir");
+    homePage.passwordTextBox.sendKeys(ConfigReader.getProperty("scAdminPassword"));
+
+    extentTest.info("Login Butonuna tıkla");
+    homePage.loginButonu.click();
+
+
+    extentTest.info("Plan Sekmesine tıkla");
+    homePage.planSekmesi.click();
+
+    ReusableMethods.wait(2);
+
+    extentTest.pass("İlk üyelik planı görünür");
+
+    Assert.assertTrue(homePage.uyelikPlani.isDisplayed());
+
+
+
+
+
+
+
 
 }
 
